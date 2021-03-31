aee3ae353c832384b7fdf669d12ed04012053d4c
uLatipov <latipovusmonkhon2006@gmail.com>
2021-03-31 19:03:45 +0500

    HAMBURGER

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  $(".menuToggle").click(function() {
+    $(this).toggleClass("active");
+    $('.menu').slideToggle(300, function(){
+      if($(this).css('display') === "none"){
+        $(this).removeAttr('style');
+      }
+    });
+  });
+});
